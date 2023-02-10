3c23692ff848af66dd27c0c2cfc3252eb877d149
Bob-A-Dook <bob.adook@tutanota.com>
2023-02-10 01:02:28 +0100

    Multiple improvements to comdiff: adaptations to new Facebook post display mode (as overlay); function for opening comment report right after saving; adding post author's name to output file names; autoremoving huge empty spaces after images

--- assets/skrypty/comdiff.py
+++ assets/skrypty/comdiff.py
@@ -24,15 +24,16 @@ __license__ = 'MIT'
 # ^ In short: just use it however you want ;) No strings attached.
 # However, I take no responsibility for anything you do.
 
-##########
-# Imports and basic 
-##########
+############################
+# Imports and basic settings
+############################
 
 import re
 from time import time
 from pathlib import Path
 from logging import warning, error, exception, basicConfig
 from shutil import which as program_exists
+from webbrowser import open as open_in_browser
 from urllib.parse import urlparse
 
 basicConfig( format='\n%(message)s\n' )
@@ -41,6 +42,7 @@ def show( text ):
     print('', text, '', sep='\n')
 
 ERR = ''
+ADD_POST_AUTHOR_TO_FILENAME = True
 
 ##################################
 # Importing BeautifulSoup for HTML
@@ -160,7 +162,7 @@ def _remove_tracking_params( link ):
     
     l = urlparse(link)
     params = [p for p in l.query.split('&')
-              if not p.startswith('__cft__[0]')
+              if not p.startswith('__cft__[') # Not just [0] since 2023
               and not p.startswith('__tn__')]
     params = '&'.join( params )
     params = f'?{params}' if params else ''
@@ -247,6 +249,8 @@ COMMENT_RE = re.compile('(komentarz|odpowiedź)')
 
 def get_comments( html ):
     '''Uses BeautifulSoup to find individual comments'''
+    
+    if not html: return
 
     coms = html.find_all('div', attrs={'aria-label':True, 'role':'article'})
     coms = [c for c in coms
@@ -254,7 +258,7 @@ def get_comments( html ):
     if not coms:
         warn = 'Nie znaleziono żadnych komentarzy!'
         warning( warn )
-    else: print(f'\n[OK]\nUzyskano komentarze! Ich liczba: {len(coms)}')
+    else: print(f'\n[OK]\nUzyskano komentarze! Ich liczba: {len(coms)}\n')
 
     coms = [ FbComment(c) for c in coms ]
     return coms
@@ -295,6 +299,7 @@ def find_censored( post1, post2 ):
     if len(comments1) == len(comments2):
         print('[OK]\nBrak różnicy między trybem zwykłym a cenzurowanym!')
         return []
+        #all_coms, reduced = comments1, comments2
 
     if len(comments1) > len(comments2):
         all_coms, reduced = comments1, comments2
@@ -317,9 +322,9 @@ def find_censored( post1, post2 ):
         warning('Niektóre komentarzy nie były rozwinięte, ich treść będzie '
                 'niepełna')
 
-    uncen_num = len(annotated)
-    print(f'Wśród {uncen_num} komentarzy było '
-          f'{uncen_num-len(reduced)} domyślnie ukrytych')
+    all_coms_num = len(annotated)
+    print(f'Wśród {all_coms_num} komentarzy było '
+          f'{all_coms_num-len(reduced)} domyślnie ukrytych')
     return annotated
 
 
@@ -333,6 +338,24 @@ def _find_censored_in_files( file1, file2 ):
 # Saving comments as HTML on disk
 #################################
 
+def _remove_large_vertical_padding( post ):
+    '''
+    For some reason, there are huge gaps under images embedded in posts.
+    This function removes them.
+    '''
+    tags_with_style_attr = post.find_all( True, attrs={'style':True})
+    padding_finder = re.compile('padding-bottom: .*?;')
+    
+    for tag in tags_with_style_attr:
+        style_info = tag.attrs['style']
+        print('S', style_info)
+        pad = padding_finder.search( style_info )
+        if pad:
+            full_pad_info = pad.group(0)
+            style_info = style_info.replace( full_pad_info,'' )
+            tag.attrs['style'] = style_info
+
+
 def _hide_options_bar( comment ):
     '''Hides some controls which would be useless in an offline file'''
     options = comment.find('ul')
@@ -344,12 +367,20 @@ def _hide_options_bar( comment ):
 def save_html_summary( annotated_comments, post1, post2 ):
     '''Saves censored comments to a file for further inspection'''
     
-    short_author = '' #TODO: Get from main link
-    filename = f'cenzura_{int(time())}.html'
+    post_author = post1.author
+
+    if post_author and ADD_POST_AUTHOR_TO_FILENAME:
+        #Keep letters and numbers only to avoid filename errors
+        post_author = '_'.join( re.findall('[a-zA-Z0-9]+', post_author) )
+        filename = f'cenzura_{int(time())}_{post_author}.html'
+    else:
+        filename = f'cenzura_{int(time())}_facebook.html'
 
     formatted = []
     for c, was_censored in annotated_comments:
         _hide_options_bar( c.html )
+        _remove_large_vertical_padding( c.html )
+        
         text = str(c.html)
         c_class = 'fb-comment'
         if was_censored: c_class += ' censored'
@@ -371,6 +402,7 @@ def save_html_summary( annotated_comments, post1, post2 ):
     with open( outpath, 'w') as out: out.write( content )
     
     show(f'[OK]\nZapisano oznaczone komentarze do pliku:\n{outpath}')
+    return outpath
     
 ###########
 # Main loop
@@ -464,8 +496,9 @@ def _find_link_to_main_post( links_with_tags ):
         is_from_fb = ('facebook.com' in link)
         is_link_to_post = ('/posts/' in link or 'pfbid' in link)
         is_comment = ('comment_id' in link)
+        is_complete = (not link.endswith('.php'))
         
-        if is_from_fb and is_link_to_post:
+        if is_from_fb and is_link_to_post and is_complete:
             if is_comment:
                 post_link = _extract_main_post_link( link )
                 warning(f'[UWAGA] Link do głównego posta nie był podany '
@@ -476,23 +509,41 @@ def _find_link_to_main_post( links_with_tags ):
     return post_link
 
 
-def _get_post_author( post_link, links_with_tags ):
+def _get_post_author( post, post_link, links_with_tags ):
     '''
-    Gets the author of the post. Usually it's enough to just trim the
-    post link, but it's more nuanced if it was published within a group.
+    Gets link to the author of the post. Usually it's enough to just trim the
+    post link, but it's more nuanced if it was published within a group
+    or if there is no path, but rather a PHP script with parameters.
     '''
     author = None
+
+    if '.php' in post_link:
+        # The author link will probably be distinct from the post link,
+        # we have to search for it separately
+        bold_links = [a for a in post.find_all('a', attrs={'href':True})
+                      if a.find('strong')]
+        if bold_links:
+            if len(bold_links) > 1:
+                warning('[UWAGA] Więcej niż jeden element mógłby wskazywać '
+                        'autora posta, skrypt wybierze pierwszy z nich.')
+                
+            author_link = bold_links[0].attrs['href']
+            return _remove_tracking_params( author_link )
+
+    # Apart from this special case, we can usually get author from post link
     if (not '/groups/' in post_link and '/posts/' in post_link):
         return re.sub('/posts/.*?$', '', post_link )
 
+    # Otherwise, try to find the author through comments
     for tag, link in links_with_tags:
         if link.startswith('/'):
             link = 'https://www.facebook.com'+link
         if '/groups/' in link and '/user/' in link:
             return link
 
-    if not author: warning('Nie udało się ustalić linku do autora posta')
-    return 'UNKNOWN'
+    if not author:
+        warning('[BRAK] Nie udało się ustalić linku do autora posta')
+    return ''
 
 
 def _extract_main_post_link( comment_link ):
@@ -505,7 +556,7 @@ def _get_post_data( post ):
     Gets data which identifies the first post so that the script can
     later warn about comment source mismatches.
     '''
-    post_data = {'author':None, 'link': None}
+    post_data = {'author':None, 'author_link': None, 'link': None}
 
     link_elems = post.find_all('a', attrs={'href':True})
     links_with_tags = [(le, le.attrs['href']) for le in link_elems]
@@ -518,20 +569,72 @@ def _get_post_data( post ):
         post_data['link'] = post_link
         print(f'Ustalony link do posta:\n{post_link}')
         
-        author = _get_post_author( post_link, links_with_tags )
-        post_data['author'] = author
-        print(f'Ustalony link do autora posta:\n{author}')
+        author_link = _get_post_author( post, post_link, links_with_tags )
+        post_data['author_link'] = author_link
+        
+        if author_link:
+            print(f'Ustalony link do autora posta:\n{author_link}')
+            res = re.search( 'www.facebook.com/(.*?)$', author_link)
+            if res: post_data['author'] = res.group(1)
 
     return post_data
 
 
+def _get_comment_number( post ):
+    '''
+    Gets the number of comments posted under a post. Later used for
+    checking whether there have been any new ones added between the captures,
+    which could distort the statitstics.
+    '''
+    headers = post.find_all('h3')
+    com_num = None
+    for h in headers:
+        text = h.string
+        if text:
+            res = re.search('(\d+) komentarz', text)
+            if res: return int( res.group(1) )
+
+    if not com_num:
+        warning('[BRAK] Nie ustalono liczby komentarzy pod postem')
+    return com_num
+
+
+def _get_display_mode( post ):
+    '''
+    Checks whether the post has all comments displayed or only
+    the most relevant ones. Not that important, but allows the script
+    to provide more accurate instructions or avoid unneeded work.
+    '''
+    KNOWN_LABELS = ('Najtrafniejsze', 'Najpopularniejsze komentarze',
+                    'Wszystkie', 'Wszystkie komentarze')
+    dmode = None
+    buttons = post.find_all('div', attrs={'role':'button'})
+    for btn in buttons:
+        has_arrow = btn.find('i', attrs={'data-visualcompletion':'css-img'})
+        if has_arrow:
+            text = btn.text.strip()
+            # Now a weird hack; the text has a special sig character which
+            # does not get removed by strip(), so I grab letters with regex
+            if text:
+                text = re.search('[A-Za-z ]+', text).group(0)    
+                if text in KNOWN_LABELS:
+                    dmode = text
+                    break
+                      
+    if not dmode: warning('[BRAK] Nie ustalono trybu wyświetlania komentarzy')
+    return dmode
+    
+
 class FacebookPost:
     '''A class corresponding to a Facebook post along with its comments'''
 
-    def __init__(self, html_post ):
+    mode = None
+
+    def __init__(self, html_post, comments ):
             
         data = _get_post_data( html_post )
-        comments = get_comments( html_post )
+        all_comment_num = _get_comment_number( html_post )
+        display_mode = _get_display_mode( html_post )
 
         # Check if all comments under the post have been unrolled
         clickables = html_post.find_all('div', attrs={'role':'button'})
@@ -554,8 +657,11 @@ class FacebookPost:
 
         self.html = html_post
         self.data = data
+        self.author = self.data['author']
         self.comments = comments
         self.is_incomplete = (rolled or rolled_replies)
+        self.mode = display_mode
+        self.all_comment_num = all_comment_num
 
 ###############
 # Sanity checks
@@ -614,7 +720,7 @@ def _display_greeting():
 
     _display_help()
           
-    show('[UWAGA] Program działa tylko na stronie Facebooka w polskiej '
+    show('[WAŻNE] Program działa tylko na stronie Facebooka w polskiej '
          'i angielskiej wersji językowej.')
 
 
@@ -625,22 +731,74 @@ def _display_help():
          'Z - powrót do poprzedniego etapu.\n'
          'F - porównanie dwóch zapisanych plików HTML z aktywnego '
           'folderu zamiast ze schowka.')
+
+
+def _show_no_clip_access_error():
+    error('Brak dostępu do schowka, program nie zadziała w tym trybie.\n'
+          'Ale nadal możesz zapisać pliki HTML do tego folderu co skrypt '
+          'i je ze sobą porównać.\nNaciśnij w tym celu klawisz F i Enter')
     
 
 def _prompt_for_input( post1, post2 ):
     '''
-    Asks user for input. Shows different things depending on the
-    current state.
+    Asks user for input. Shows different things depending on how many
+    posts have been analyzed by the script.
     '''    
     if not post1:
         show('[1] Skopiuj do schowka źródło HTML jakiegoś posta '
               'z Facebooka i naciśnij Enter.')
     else:
-        show('[2] Teraz wybierz opcję "Wszystkie komentarze" pod tym '
+        other_post_mode = 'Wszystkie komentarze'
+        if post1.mode:
+            if post1.mode == 'Wszystkie':
+                other_post_mode = 'Najtrafniejsze'
+                  
+        show(f'[2] Teraz wybierz opcję "{other_post_mode}" pod tym '
               'samym postem, skopiuj jego kod HTML i naciśnij Enter.')
         
     inp = input('> ')
     return inp
+
+
+def _finalize_and_show_prompt( post1, post2 ):
+    '''
+    After the comments from both modes (relevant and all) have been gathered,
+    they are compared to find examples of censorship.
+    The user is then  asked whether they want to quit, display the comparison
+    or go back to the previous step
+    '''
+    censored = find_censored( post1, post2 )
+            
+    saved_file = None
+    if censored and SAVE_REPORT:
+        saved_file = save_html_summary( censored, post1, post2 )
+
+    m = ('[KONIEC] Możesz nacisnąć Enter, żeby opuścić program.\n'
+         'Możesz też nacisnąć Z i Enter, żeby wrócić do poprzedniego kroku')
+    if saved_file:
+        m += (', albo O, żeby otworzyć zapisane komentarze w przeglądarce')
+              
+    show(m)
+    user_choice = input('Twój wybór: ')
+    
+    return user_choice, saved_file
+
+
+def __search_for_post( html, tag_name, attrs ):
+    '''
+    Searches the HTML code for a matching tag which should correspond
+    to a Facebook post. If there are no comments, however, the post
+    is not returned.
+    '''
+    post_info = None
+
+    post = html.find( tag_name, attrs )
+    if post:
+        comments = get_comments( post )
+        if comments:
+            post_info = (post, comments)
+
+    return post_info
 
 
 def _get_facebook_post( clip_text, post1 ):
@@ -666,26 +824,48 @@ def _get_facebook_post( clip_text, post1 ):
     # Try various ways of getting Facebook post (different formats on
     # timeline, on pages, depends on the method of copying HTML...)
     
-    post = None
-    if 'aria-posinset' in html.attrs: post = html
+    post_info = None
 
-    if not post:
-        post = html.find( True, attrs={'aria-posinset': True} )
+    # From 2023 onwards, the post opens in an overlay; use this
+    # as the default check
+    
+    if 'role' in html.attrs and html.attrs['role'] == 'dialog':
+        post = html
+        comments = get_comments( post )
+        if comments: post_info = (post, comments)
 
-    if not post:
+    if 'aria-posinset' in html.attrs:
+        post = html
+        comments = get_comments( post )
+        if comments: post_info = (post, comments)
+
+    # If the copied HTML element does not have the necessary attributes,
+    # then search inside it. Perhaps some parent tag was selected instead
+    
+    if not post_info:
+        post_info = __search_for_post( html, 'div', attrs={'role': 'dialog'})
+
+    if not post_info:
+        post_info = __search_for_post( html, True, attrs={'aria-posinset': True} )
+
+    if not post_info:
         if ('role' in html.attrs and html.attrs['role'] == 'article'):
             post = html
-        else: post = html.find('div', attrs={'role':'article'})
-    
-    if not post:
-        post = html
-        warning('[UWAGA] Nie znaleziono posta w odpowiednim formacie. '
-                'Facebook mógł go zmienić. Program spróbuje poszukać '
-                'tego co zwykle, ale może nie zadziałać.\n'
-                'Dla pewności sprawdź, czy kopiujesz cały element '
-                'zawierający post, a nie tylko pojedyncze komentarze')
+            comments = get_comments( post )
+            if comments: post_info = (post, comments)
 
-    return FacebookPost( post )
+    if not post_info:
+        post = html
+        comments = get_comments( post )
+        warning('[UWAGA]\nPrzeszukano kod HTML na kilka sposobów, ale nie '
+                'udało się znaleźć posta z komentarzami. '
+                'Facebook mógł zmienić format postów na stronie.\n'
+                'Dla pewności sprawdź, czy kopiujesz cały element '
+                'zawierający post, a nie tylko pojedyncze komentarze.\n'
+                'Możesz np. skopiować post przez narzędzia przeglądarki')
+
+    post, comments = post_info
+    return FacebookPost( post, comments )
     
 
 def run_interactive_mode():
@@ -701,45 +881,47 @@ def run_interactive_mode():
 
         inp = _prompt_for_input( post1, post2 ).lower()
         
-        if inp == 'q':
-            return
+        if inp == 'q': return
+            
         elif inp == 'f':
             return find_censored_in_files()
+
+        elif inp == 'h':
+            _display_help()
+            continue
         
         elif inp == 'z':
             post1 = None
             continue
         
-        elif inp == 'h':
-            _display_help()
-            continue
-
+        # If some other key was pressed, we try to get post from clipboard
+        
         if not clipboard_get:
-            error('Brak dostępu do schowka, program nie zadziała w tym '
-                  'trybie.\nAle nadal możesz zapisywać pliki HTML w jednym '
-                  'folderze i je ze sobą porównywać')
-            return
+            _show_no_clip_access_error()
+            continue
 
         clip_text = clipboard_get()
         post = _get_facebook_post( clip_text, post1 )
         if not post: continue
             
-        # Getting and analyzing posts along with their comments
         if not post1: post1 = post
         else: post2 = post
-            
-        if post1 and post2:
-            censored = find_censored( post1, post2 )
-            if censored and SAVE_REPORT:
-                save_html_summary( censored, post1, post2 )
 
-            show('[KONIEC] Możesz teraz nacisnąć Z, żeby wrócić do '
-                 'poprzedniego kroku, albo Enter, żeby opuścić program.')
-            inp2 = input('Twój wybór: ')
+        # After we get two proper posts with comments, we can compare them
+        
+        if post1 and post2:
+            
+            inp2, saved_file = _finalize_and_show_prompt( post1, post2 )
+            
             if inp2.lower() == 'z':
                 post2 = None
-            else:
+                continue
+            
+            elif inp2.lower() == 'o' and saved_file:
+                open_in_browser( str(saved_file) )
                 return
+
+            else: return
     
     
 if __name__ == '__main__':
